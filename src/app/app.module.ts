31595d808118a127f12938b7c5f0a7e246df98da
unknown <you@example.comdisasterz9955@gmail.com>
2018-09-09 22:34:23 +0300

    v2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { MoviesComponent } from './movies/movies.component';
+import { MovieComponent } from './movie/movie.component';
+import { SeriesComponent } from './series/series.component';
+import { SerieComponent } from './serie/serie.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    MoviesComponent,
+    MovieComponent,
+    SeriesComponent,
+    SerieComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    RouterModule.forRoot ([
+      { path: '', component: HomeComponent },
+      { path: 'movies', component: MoviesComponent },
+      { path: 'series', component: SeriesComponent },
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent },
+      { path: 'movie', component: MovieComponent },
+      { path: 'serie', component: SerieComponent },
+      { path: 'profile', component: ProfileComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
